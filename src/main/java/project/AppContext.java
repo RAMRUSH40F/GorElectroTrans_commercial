30c382c69cf281d57742ca94f50fc07d21a6ee9e
ramru <ramrush-r@mail.ru>
2023-02-24 17:46:05 +0300

    Базированный на Maven проект. Начальная конфигурация.

---
+++ src/main/java/project/AppContext.java
@@ -0,0 +1,12 @@
+package project;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppContext {
+    //Здесь будем создавать бины
+
+
+
+
+}
---
+++ src/main/java/project/controller/SomeDataController.java
@@ -0,0 +1,9 @@
+package project.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SomeDataController {
+    // Здесь будем отдавать данные
+
+}
