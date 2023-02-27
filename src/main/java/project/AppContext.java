b7263f7ab10b4407be3e379ad5e2daa9bd24d430
ramru <ramrush-r@mail.ru>
2023-02-27 18:03:33 +0300

    Добавил доступ к базе данных через готовый бин jdbcTemplate.

--- src/main/java/project/AppContext.java
+++ src/main/java/project/AppContext.java
@@ -1,12 +1,35 @@
 package project;
 
+import org.mariadb.jdbc.MariaDbDataSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
 
 @Configuration
 public class AppContext {
-    //Здесь будем создавать бины
 
+    @Bean("mariaDB")
+    public DataSource mariaDataSource() throws SQLException {
+        MariaDbDataSource mariaDbDataSource = new MariaDbDataSource();
+        mariaDbDataSource.setUrl("jdbc:mariadb://82.146.38.158:3307");
+        mariaDbDataSource.setUser("root");
+        mariaDbDataSource.setPassword("");
+        return mariaDbDataSource;
+    }
 
+    @Bean
+    public JdbcTemplate myJdbcTemplate(DataSource mariaDataSource) {
+        return new JdbcTemplate(mariaDataSource);
+    }
+
+    @Bean
+    public NamedParameterJdbcTemplate myNamedParameterJdbcTemplate(DataSource mariaDataSource) {
+        return new NamedParameterJdbcTemplate(mariaDataSource);
+    }
 
 
 }
--- src/main/java/project/Application.java
+++ src/main/java/project/Application.java
@@ -3,12 +3,15 @@ package project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import project.repository.temp.LessonPopulator;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+
+        context.getBean("LessonPopulatorBean", LessonPopulator.class).addOneLesson();
 
 
     }
