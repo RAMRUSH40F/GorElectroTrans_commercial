8b6e576815e7a60baa2fae27a91a372344153963
Artem Shevtsov <shevtsov.ae@edu.spbstu.ru>
2023-03-01 22:01:01 +0300

    Добавлен ресурсный файл с тестовыми именами работников

--- src/main/java/project/AppContext.java
+++ src/main/java/project/AppContext.java
@@ -3,6 +3,7 @@ package project;
 import org.mariadb.jdbc.MariaDbDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
@@ -10,6 +11,7 @@ import javax.sql.DataSource;
 import java.sql.SQLException;
 
 @Configuration
+@PropertySource("classpath:stringRes.properties")
 public class AppContext {
 
     @Bean("mariaDB")
