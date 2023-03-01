ca5e697b8f900497533976bb61960cf9e849fc1b
ramru <ramrush-r@mail.ru>
2023-03-01 23:26:25 +0300

    Сделал рефакторинг кода WorkersMultiplier и WorkersRepository(просто отступы в билдере и ctrl+alt+L и по мелочи).

--- src/main/java/project/repository/WorkersRepository.java
+++ src/main/java/project/repository/WorkersRepository.java
@@ -10,22 +10,24 @@ import java.util.Map;
 
 @Repository
 public class WorkersRepository {
-@Autowired
+    @Autowired
     NamedParameterJdbcTemplate namedParameterJdbcTemplate;
- public void addNewWorker(Worker worker){
-     String id= worker.getId();
-     if(id.length()>5)
-         throw new IndexOutOfBoundsException("Too many symbols in ID!!");
-     for(int i=0;i<5;i++){
-         if(id.charAt(i)<48||id.charAt(i)>57) // Смотрим кодировку сиволов id
-             throw new RuntimeException("Incorrect id symbol!!!!");
-     }
-     Map<String,Object> workerData=new HashMap<>();
-     workerData.put("id", worker.getId());
-     workerData.put("name",worker.getName());
-     namedParameterJdbcTemplate.update("INSERT INTO WORKERS.workers(id,name)"
-             +"VALUES(:id,:name)",workerData);
 
- }
+    public void addNewWorker(Worker worker) {
+        String id = worker.getId();
+        if (id.length() > 5)
+            throw new IndexOutOfBoundsException("Too many symbols in ID!!");
+        for (int i = 0; i < 5; i++) {
+            if (id.charAt(i) < 48 || id.charAt(i) > 57) // Смотрим кодировку сиволов id
+                throw new RuntimeException("Incorrect id symbol!!!!");
+        }
+        Map<String, Object> workerData = new HashMap<>();
+        workerData.put("id", worker.getId());
+        workerData.put("name", worker.getName());
+        namedParameterJdbcTemplate.update(
+                "INSERT INTO WORKERS.workers(id,name) VALUES(:id,:name)",
+                    workerData);
+
+    }
 
 }
--- src/main/java/project/repository/multiplier/WorkersMultiplier.java
+++ src/main/java/project/repository/multiplier/WorkersMultiplier.java
@@ -11,30 +11,34 @@ import java.util.Random;
 @Service("WorkersMultiplierBean")
 public class WorkersMultiplier {
     @Autowired
-   Environment env;
+    Environment env;
 
     @Autowired
-  WorkersRepository workersRepository;
-  private final StringBuilder idStringBuilder =new StringBuilder();
-    public void addAllWorkers() {
-        Random random=new Random();
+    WorkersRepository workersRepository;
 
-    for(int secondN=0;secondN<5;secondN++){
-        for(int thirdN=0;thirdN<10;thirdN++){
-            for(int fourthN=0;fourthN<10;fourthN++){
-                for(int fithtN=0;fithtN<10;fithtN++){
-                    idStringBuilder.delete(0,5);
-                    idStringBuilder.append(0).append(secondN).append(thirdN).append(fourthN).append(fithtN);
-                    Worker worker = Worker.builder().id(idStringBuilder.toString()).name(env.getProperty("name"+(random.nextInt(20)+1))).build();
-                    workersRepository.addNewWorker(worker);
+
+    public void addAllWorkers() {
+        StringBuilder idBuilder = new StringBuilder();
+        Random random = new Random();
+
+        for (int secondN = 0; secondN < 5; secondN++) {
+            for (int thirdN = 0; thirdN < 10; thirdN++) {
+                for (int fourthN = 0; fourthN < 10; fourthN++) {
+                    for (int fithtN = 0; fithtN < 10; fithtN++) {
+                        idBuilder.delete(0, 5);
+                        idBuilder.append(0).append(secondN).append(thirdN).append(fourthN).append(fithtN);
+
+                        Worker worker = Worker.builder()
+                                .id(idBuilder.toString())
+                                .name(env.getProperty("name" + (random.nextInt(20) + 1)))
+                                .build();
+                        workersRepository.addNewWorker(worker);
+                    }
                 }
             }
         }
-    }
-System.out.println("Script ended his Work!");
 
     }
-
 
 
 }
