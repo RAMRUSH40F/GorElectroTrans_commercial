fb7a21d1f1171b0b8dce5c61d932cbcaade0ab7d
ramru <ramrush-r@mail.ru>
2023-03-03 19:04:15 +0300

    Изменил сигнатуру метода StudentMultiplier.addNewStudent

--- src/main/java/project/Application.java
+++ src/main/java/project/Application.java
@@ -14,11 +14,17 @@ public class Application {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
 
-        context.getBean("LessonMultiplierBean", LessonMultiplier.class).addOneLesson();
-//context.getBean("WorkersMultiplierBean", WorkersMultiplier.class).addAllWorkers();
-//context.getBean("SubDepartmentBean", SubDepartmentMultiplier.class).addSubDepartments();
-        context.getBean("StudentMultiplierBean", StudentMultiplier.class).addNewStudent();
-context.close();
+
+        // Заполняем при запуске таблицы студентов тестовыми данными.
+        //context.getBean("WorkersMultiplierBean", WorkersMultiplier.class).addAllWorkers();
+
+        StudentMultiplier studentMultiplier = context.getBean("StudentMultiplierBean", StudentMultiplier.class);
+        //Заполняем при запуске таблицы студентов тестовыми данными.
+        for (int i = 1; i < 16; i++) {
+            studentMultiplier.addNewStudent(i);
+        }
+
+        context.close();
     }
 
 }
--- src/main/java/project/repository/multiplier/StudentMultiplier.java
+++ src/main/java/project/repository/multiplier/StudentMultiplier.java
@@ -12,18 +12,19 @@ public class StudentMultiplier {
     @Autowired
     StudentRepository studentRepository;
 
-    public void addNewStudent() {
+    public void addNewStudent(int DatabaseId) {
         Random random = new Random();
-        short departmentId = (short) ( (random.nextInt(6) + 1)*100+1);
-        StringBuilder idBuilder=new StringBuilder();
-        idBuilder.append(0).append(random.nextInt(5));
-        for(int i=0;i<3;i++){
+        StringBuilder idBuilder = new StringBuilder();
+        idBuilder.append(0);
+        idBuilder.append(random.nextInt(5));
+        for (int i = 0; i < 3; i++){
             idBuilder.append(random.nextInt(10));
         }
+        short departmentId = (short) ((random.nextInt(6) + 1) * 100 + 1);
         Student student = Student.builder()
-                .student_id(idBuilder.toString())
-                .department_id(departmentId)
+                .studentId(idBuilder.toString())
+                .subDepartmentId(departmentId)
                 .build();
-        studentRepository.addNewStudent(1, student);
+        studentRepository.addNewStudent(DatabaseId, student);
     }
 }
