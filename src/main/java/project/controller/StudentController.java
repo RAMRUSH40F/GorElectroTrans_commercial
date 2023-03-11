5d75122bc578c9551662e5e2422b949e3c3090ac
ramru <ramrush-r@mail.ru>
2023-03-11 15:38:10 +0300

    Написал все методы контроллера.
    Извините за не информативность, я писал ТЗ.

--- src/main/java/project/controller/StudentController.java
+++ src/main/java/project/controller/StudentController.java
@@ -2,6 +2,7 @@ package project.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import project.exceptions.InvalidStudentIdException;
 import project.model.StudentView;
 import project.repository.StudentRepository;
 
@@ -13,12 +14,10 @@ public class StudentController {
     @Autowired
     StudentRepository studentRepository;
 
-    @GetMapping("/dep_{n}/students/data")
-    public List<StudentView> getAllStudents(@PathVariable("n") String dep_n) {
+    @GetMapping("/dep_{N}/students/data")
+    public List<StudentView> getAllStudents(@PathVariable("N") int departmentId) {
         // Проверка на роль.
         boolean isSuperAdmin = true;
-
-        int departmentId = Integer.parseInt(dep_n);
 
         if (isSuperAdmin) {
             return studentRepository.getStudentsAdminView(departmentId);
@@ -28,13 +27,47 @@ public class StudentController {
 
     }
 
+    @GetMapping("/dep_{N}/students/{id}")
+    public StudentView findStudentById(@PathVariable("N") int departmentId, @PathVariable("id") String studentId) {
 
-    @PostMapping("/")
-    public void addNewStudent() {
+        if (studentId.length() != 5) {
+            throw new InvalidStudentIdException();
+        }
+        // Проверка на роль.
+        boolean isSuperAdmin = true;
+
+        if (isSuperAdmin) {
+            return studentRepository.getStudentByIdAdmin(departmentId, studentId);
+        } else {
+            return studentRepository.getStudentById(departmentId, studentId);
+        }
 
     }
 
-    @DeleteMapping("/")
-    public void deleteStudentById() {
+    @PostMapping("/dep_{N}/students/data")
+    public void addNewStudent(@RequestBody StudentView student, @PathVariable("N") int departmentId) {
+        studentRepository.addNewStudent(departmentId, student);
+    }
+
+    @PutMapping("/dep_{N}/students/data")
+    public void updateStudent(@RequestBody StudentView student, @PathVariable("N") int departmentId) {
+        studentRepository.updateStudent(departmentId, student);
+    }
+
+    @DeleteMapping("/dep_{N}/students/{id}")
+    public void deleteStudentById(@PathVariable("N") int departmentId, @PathVariable("id") String studentId) {
+        if (studentId.length() != 5) {
+            throw new InvalidStudentIdException();
+        }
+        // Проверка на роль.
+        boolean isSuperAdmin = true;
+
+        // Удалять студентов могут лишь супер-админы.
+        if (isSuperAdmin) {
+            studentRepository.deleteStudentById(departmentId, studentId);
+        } else {
+            throw new RuntimeException("Unauthorised person!");
+        }
+
     }
 }
