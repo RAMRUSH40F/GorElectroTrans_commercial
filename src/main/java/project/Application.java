403b06f82b18b61608707b8c30d7364fdcbe3b9b
Fonova99 <fonova99@list.ru>
2023-03-12 20:29:03 +0300

    Запуск

--- src/main/java/project/Application.java
+++ src/main/java/project/Application.java
@@ -3,10 +3,7 @@ package project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import project.repository.multiplier.LessonMultiplier;
-import project.repository.multiplier.StudentMultiplier;
-import project.repository.multiplier.SubDepartmentMultiplier;
-import project.repository.multiplier.WorkersMultiplier;
+import project.repository.multiplier.*;
 
 @SpringBootApplication
 public class Application {
@@ -14,15 +11,9 @@ public class Application {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
 
-
-        // Заполняем при запуске таблицы студентов тестовыми данными.
-        //context.getBean("WorkersMultiplierBean", WorkersMultiplier.class).addAllWorkers();
-        //заполняем подразделения
-//        context.getBean("SubDepartmentBean", SubDepartmentMultiplier.class).addAllSubDepartments();
-//      context.getBean("StudentMultiplierBean", StudentMultiplier.class).addAllStudents();
-        context.getBean("LessonMultiplierBean", LessonMultiplier.class).addAllLessons();
-        //Заполняем при запуске таблицы студентов тестовыми данными.
-
+//        context.getBean("StudentMultiplierBean", StudentMultiplier.class).addAllStudents();
+//        context.getBean("LessonMultiplierBean", LessonMultiplier.class).addAllLessons();
+        context.getBean("AttendanceMultiplierBean", AttendanceMultiplier.class).addAllAttendance();
 
         context.close();
     }
