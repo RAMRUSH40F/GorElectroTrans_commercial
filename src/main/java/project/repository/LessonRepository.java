cebf65a4ae958bada473aca691d3a2461c866554
Artem Shevtsov <shevtsov.ae@edu.spbstu.ru>
2023-03-20 18:49:06 +0300

    Обновлен репозиторий LessonRepository

--- src/main/java/project/repository/LessonRepository.java
+++ src/main/java/project/repository/LessonRepository.java
@@ -5,6 +5,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
+import project.exceptions.InvalidDepartmentException;
 import project.model.Lesson;
 
 import java.sql.ResultSet;
@@ -70,4 +71,63 @@ public class LessonRepository {
         return lessonsIdList;
     }
 
+    public List<Lesson> getAllLessonAdmin() {
+        return null;
+    }
+
+    public List<Lesson> getAllLesson(int departmentN) {
+        return namedJdbcTemplate.query("SELECT * FROM DEP_" + departmentN + ".lesson", (rs, rowNum) -> Lesson.builder().
+                id(rs.getInt("id")).
+                topic(rs.getString("topic")).
+                duration(rs.getFloat("duration")).
+                date(rs.getDate("date")).
+                teacher(rs.getString("teacher")).
+                peoplePlanned(rs.getInt("people_planned")).build());
+
+    }
+    public List<Lesson> getOneLesson(int departmentN, int id){
+        return namedJdbcTemplate.query("SELECT FROM DEP_" + departmentN + ".lesson WHERE id="+id, (rs, rowNum) -> Lesson.builder().
+                id(rs.getInt("id")).
+                topic(rs.getString("topic")).
+                duration(rs.getFloat("duration")).
+                date(rs.getDate("date")).
+                teacher(rs.getString("teacher")).
+                peoplePlanned(rs.getInt("people_planned")).build());
+
+    }
+    public void  changeLesson(int department, int id, Lesson changed_lesson){
+//Map<String, Object> lessonMap=new HashMap<>();
+
+        String query = new StringBuilder()
+                .append("UPDATE DEP_")
+                .append(department)
+                .append(".lesson SET topic=")
+                .append(changed_lesson.getTopic())
+                .append(", duration=")
+                .append(changed_lesson.getDuration())
+                .append(", date")
+                .append(changed_lesson.getDate())
+                .append(", teacher=")
+                .append(changed_lesson.getTeacher())
+                .append(", people_planned=")
+                .append(changed_lesson.getPeoplePlanned())
+                .append(" WHERE id='")
+                .append(changed_lesson.getId())
+                .append("'")
+                .toString();
+
+        jdbcTemplate.execute(query);
+    }
+    public void deleteLesson(int department,int id){
+        if (department > 15 || department < 1) {
+            throw new InvalidDepartmentException("Invalid department id, it has to be in [1,15] interval");
+        }
+        String query = new StringBuilder()
+                .append("DELETE FROM DEP_")
+                .append(department)
+                .append(".lesson WHERE id=")
+                .append(id)
+                .toString();
+        jdbcTemplate.execute(query);
+    }
 }
