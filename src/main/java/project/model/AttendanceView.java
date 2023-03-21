6b856c52ca9644241e29f2e6f953d9500fcce920
Fonova99 <fonova99@list.ru>
2023-03-21 19:27:43 +0300

    Добавление класса AttendanceView

---
+++ src/main/java/project/model/AttendanceView.java
@@ -0,0 +1,20 @@
+package project.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AttendanceView {
+    @JsonProperty("lesson_id")
+    private Integer lessonId;
+    @JsonProperty("student_id")
+    private String studentId;
+    @JsonProperty("success")
+    private int success;
+}
