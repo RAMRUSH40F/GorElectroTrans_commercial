387b4dcad0aadf1fe1b6526c3a8bca92482ee9ed
Fonova99 <fonova99@list.ru>
2023-03-21 19:26:20 +0300

    Добавление класса AttendanceController

---
+++ src/main/java/project/controller/AttendanceController.java
@@ -0,0 +1,43 @@
+package project.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import project.exceptions.InvalidStudentIdException;
+import project.model.AttendanceView;
+import project.model.StudentView;
+import project.repository.AttendanceRepository;
+
+import java.util.List;
+
+@RestController("AttendanceControllerBean")
+public class AttendanceController {
+
+    @Autowired
+    AttendanceRepository attendanceRepository;
+
+    @GetMapping("/dep_{N}/attendance/data")
+    public List<AttendanceView> getAllAttendances(@PathVariable("N") int departmentId) {
+        return attendanceRepository.getAllAttendances(departmentId);
+    }
+
+    @GetMapping("/dep_{N}/attendance/{id}}")
+    public AttendanceView getRecordAttendanceById(@PathVariable("N") int departmentId, @PathVariable String id) {
+        return attendanceRepository.getRecordAttendanceById(departmentId, id);
+    }
+
+//    @PostMapping("/dep_{N}/attendance/data")
+//    public void addNewRecordAttendance(@PathVariable("N") int departmentId, @RequestBody AttendanceView attendanceView) {
+//        attendanceRepository.addNewRecordAttendance(departmentId , attendanceView);
+//    }
+
+    @PutMapping("/dep_{N}/attendance/data")
+    public void updateRecordAttendance(@PathVariable("N") int departmentId, @RequestBody AttendanceView attendanceView) {
+
+    }
+
+    @DeleteMapping("/dep_{N}/attendance/{id}")
+    public void deleteRecordById(@PathVariable("N") int departmentId, @PathVariable("id") String studentId) {
+        attendanceRepository.deleteRecordById(departmentId, studentId);
+    }
+
+}
