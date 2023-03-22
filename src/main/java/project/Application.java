4eeba31a89bccaf262da1afc2a941aa25b807c27
Artem Shevtsov <shevtsov.ae@edu.spbstu.ru>
2023-03-22 21:23:05 +0300

    Подправил Application

--- src/main/java/project/Application.java
+++ src/main/java/project/Application.java
@@ -12,7 +12,6 @@ public class Application {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
-
     }
 
 }
