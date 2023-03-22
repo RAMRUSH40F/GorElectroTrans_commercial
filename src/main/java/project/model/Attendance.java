47e5b9952206cac8a3187b04b010ea93ecdb6d89
Fonova99 <fonova99@list.ru>
2023-03-22 20:16:38 +0300

    Добавление аннотации @JsonProperty и @NoArgsConstructor

--- src/main/java/project/model/Attendance.java
+++ src/main/java/project/model/Attendance.java
@@ -1,15 +1,21 @@
 package project.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor
 public class Attendance {
+    @JsonProperty("lesson_id")
     private Integer lessonId;
+    @JsonProperty("student_id")
     private String studentId;
+    @JsonProperty("success")
     private int success;
 
 }
