9b527ad3255ef6c536c691c052f6e00f8c896c72
ramru <ramrush-r@mail.ru>
2023-03-26 14:36:45 +0300

    Расширена область применения для InvalidStudentIdException

--- src/main/java/project/exceptions/InvalidStudentIdException.java
+++ src/main/java/project/exceptions/InvalidStudentIdException.java
@@ -3,15 +3,24 @@ package project.exceptions;
 public class InvalidStudentIdException extends RuntimeException {
 
     private final Object receivedId;
-    private final String messageTemplate = "Вы ввели неправильны id обучающегося." +
+    private final String MESSAGE_TEMPLATE = "Вы ввели неправильны id обучающегося." +
             " id - набор из 5 цифр 00000-99999. Вы ввели: ";
+    private String customMessage;
 
-    public InvalidStudentIdException(Object receivedId) {
+    public InvalidStudentIdException(String receivedId, String customMessage) {
+        this.receivedId = receivedId;
+        this.customMessage = customMessage;
+    }
+
+    public InvalidStudentIdException(String receivedId) {
         this.receivedId = receivedId;
     }
 
     @Override
     public String getMessage() {
-        return messageTemplate + receivedId;
+        if (customMessage == null) {
+            return MESSAGE_TEMPLATE + receivedId;
+        }
+        return customMessage;
     }
 }
