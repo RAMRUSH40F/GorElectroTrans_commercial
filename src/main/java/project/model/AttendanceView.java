14fd06b71603b059ae9755e779cf9c847ccfab8d
Fonova99 <fonova99@list.ru>
2023-03-26 19:10:53 +0300

    Внесены все поля

--- src/main/java/project/model/AttendanceView.java
+++ src/main/java/project/model/AttendanceView.java
@@ -6,17 +6,29 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.sql.Date;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class AttendanceView {
+    @JsonProperty("name")
+    private String name;
     @JsonProperty("lessonId")
     private Integer lessonId;
+    @JsonProperty("date")
+    private Date date;
     @JsonProperty("studentId")
     private String studentId;
     @JsonProperty("success")
     private int success;
+    @JsonProperty("topic")
+    private String topic;
+    @JsonProperty("duration")
+    private Float duration;
     @JsonProperty("teacher")
     private String teacher;
+    @JsonProperty("subDepartment")
+    private String subDepartment;
 }
