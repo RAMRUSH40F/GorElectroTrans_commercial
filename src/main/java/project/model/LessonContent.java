22463611ed60f1467b9a0fdc1a23e3e3846f9fa4
ramru <ramrush-r@mail.ru>
2023-03-28 20:41:42 +0300

    Обновил модель

--- src/main/java/project/model/LessonContent.java
+++ src/main/java/project/model/LessonContent.java
@@ -5,15 +5,26 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.sql.Date;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class LessonContent {
 
-    private byte[] file;
+
+    private Date date;
+
+    private String topic;
 
     private Integer lessonId;
+
+    private String fileName;
+
+    private byte[] file;
+
+
 
     public static boolean isEmpty(LessonContent content){
         return content.getFile() != null && content.getLessonId() != null;
