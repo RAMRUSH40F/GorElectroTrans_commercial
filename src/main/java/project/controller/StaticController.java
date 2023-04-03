ef149c87dd457733049d40324a03947b456dd4d3
ramru <ramrush-r@mail.ru>
2023-04-03 17:33:41 +0300

    Теперь все ссылки работают

--- src/main/java/project/controller/StaticController.java
+++ src/main/java/project/controller/StaticController.java
@@ -3,14 +3,14 @@ package project.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 @Controller
 public class StaticController {
 
-    @GetMapping(value = {"/"})
-    public String getBasicHtml() {
-
+    @GetMapping(value = {"/{somepath1}", "/{somepath1}/{somepath2}"})
+    public String returnReactSPA(@PathVariable(required = false) String somepath2, @PathVariable(required = false) String somepath1) {
         return "index";
     }
 
