5d78b4c0c05f46e0dda613b7ff72d4d00487d0b1
Artem Shevtsov <shevtsov.ae@edu.spbstu.ru>
2023-04-05 23:22:34 +0300

    Поправки Отчета

--- src/main/java/project/controller/ReportController.java
+++ src/main/java/project/controller/ReportController.java
@@ -4,13 +4,13 @@ import lombok.RequiredArgsConstructor;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import project.exceptions.Validator;
 import project.model.IntervalModel;
 import project.repository.ReportRepository;
 
+import java.io.File;
 import java.time.Year;
 import java.util.ArrayList;
 import java.util.List;
@@ -24,7 +24,7 @@ public class ReportController {
     @GetMapping("/dep_{N}/report")
     public byte[] getReport(@RequestParam int interval) {
         Validator.validateInterval(interval);
-        final String filename = "src/main/resources/report_template.xlsx";
+        final String filename = "src"+ File.separator+"main"+File.separator+"resources"+File.separator+"report_template.xls";
         HSSFWorkbook workbook = reportRepository.readWorkbook(filename);
         reportRepository.formLessonReport(workbook, filename, interval);
         reportRepository.formWorkerReport(workbook, filename);
