edce2a4dd61d012d2fafb984e809c231378f7b35
ramru <ramrush-r@mail.ru>
2023-04-11 18:36:28 +0300

    Добавлена модель пользователя

---
+++ src/main/java/project/model/User.java
@@ -0,0 +1,18 @@
+package project.model;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@RequiredArgsConstructor
+public class User {
+    private final String username;
+    private final String password;
+    private final boolean isActive;
+    private final List<String> authorities;
+
+}
---
+++ src/main/java/project/repository/UserRepository.java
@@ -0,0 +1,51 @@
+package project.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.checkerframework.checker.nullness.qual.Nullable;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+import project.model.User;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository("UserRepositoryBean")
+@RequiredArgsConstructor
+public class UserRepository {
+
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    public @Nullable User getUserByUsername(String username) {
+        String GET_USER_TEMPLATE =
+                "SELECT * FROM USERS.users WHERE username=:username";
+        String GET_AUTHORITIES_TEMPLATE =
+                "SELECT authority FROM USERS.authorities WHERE username=:username";
+        Map<String, Object> userData = new HashMap<>();
+        userData.put("username", username);
+
+
+        List<String> userAuthorities = namedParameterJdbcTemplate.query(GET_AUTHORITIES_TEMPLATE, userData,
+                (rs, num) -> (rs.getString("authority")));
+        try {
+            User user = namedParameterJdbcTemplate.queryForObject(GET_USER_TEMPLATE, userData,
+                    (rs, rowNum) -> User.builder()
+                            .username(rs.getString("username"))
+                            .password(rs.getString("password"))
+                            .isActive(rs.getBoolean("enabled"))
+                            .authorities(userAuthorities)
+                            .build());
+            if (user.getUsername() != null && user.getAuthorities() != null && user.getPassword() != null) {
+                return user;
+            } else {
+                return null;
+            }
+        } catch (DataAccessException e) {
+            return null;
+        }
+
+    }
+
+
+}
