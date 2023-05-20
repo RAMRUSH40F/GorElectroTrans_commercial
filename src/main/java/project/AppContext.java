a42f432121b5024aec03c9bf8c163b576c92d1fe
ramru <ramrush-r@mail.ru>
2023-05-20 15:15:19 +0300

    Последний билд фронта

--- src/main/java/project/AppContext.java
+++ src/main/java/project/AppContext.java
@@ -7,8 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.sql.DataSource;
 import java.sql.SQLException;
@@ -48,19 +46,19 @@ public class AppContext {
      * This configuration allows Cross-Origin Resource Sharing (CORS)
      * Разкомментить, если фронт-энд ведет разработку на другом сервере нежели сервер с эндпоинтами
      */
-    @Bean
-    public WebMvcConfigurer corsConfigurer()
-    {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedOriginPatterns("*")
-                        .allowedHeaders("*")
-                        .exposedHeaders("students_count","lessons_count","attendance_count","content_count", "Authorization");
-            }
-        };
-    }
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer()
+//    {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**")
+//                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+//                        .allowedOriginPatterns("*")
+//                        .allowedHeaders("*")
+//                        .exposedHeaders("students_count","lessons_count","attendance_count","content_count", "Authorization");
+//            }
+//        };
+//    }
 
 }
