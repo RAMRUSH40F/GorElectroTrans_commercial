f648551d5c2bda0b1f4e408f94b3ed17e2c0aab6
ramru <ramrush-r@mail.ru>
2023-05-25 21:47:21 +0300

    Самое простое решение, чтобы при запуске не возникла ошибки, что база данных не выбрана

--- src/main/java/project/Application.java
+++ src/main/java/project/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import project.dataSource.DynamicDataSourceContextHolder;
 
 
 @SpringBootApplication
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 public class Application {
 
     public static void main(String[] args) {
+        DynamicDataSourceContextHolder.setCurrentDataSource("DEP_1");
         ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
     }
 
