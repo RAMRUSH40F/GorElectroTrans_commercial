67721f945af114bea0c06cb6ea87472848eded0d
Kirill Darenskii <kdarenskii@mail.ru>
2023-06-01 22:30:39 +0300

    Добавлены ограничения для времени занятия при взаимодействии с рабочим планом

--- reactFrontend/src/components/forms/PlanForm/planFormScheme.ts
+++ reactFrontend/src/components/forms/PlanForm/planFormScheme.ts
@@ -3,8 +3,8 @@ import { PlanFormValues } from ".";
 
 export const planFormScheme = yup.object<PlanFormValues>().shape({
     topic: yup.string().required("Обязательное поле").trim(),
-    duration: yup.number().required("Обязательное поле"),
-    peoplePlanned: yup.number().required("Обязательное поле"),
+    duration: yup.number().required("Обязательное поле").min(0.1, "Минимум 0.1"),
+    peoplePlanned: yup.number().required("Обязательное поле").min(1, "Минимум 1"),
     date: yup.string().required("Обязательное поле").trim(),
     teacher: yup.string().required("Обязательное поле").trim(),
 });
