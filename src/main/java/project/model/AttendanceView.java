b6fbe1d108362e803b1d14fa06b8f2cb2736d784
Artem Shevtsov <shevtsov.ae@edu.spbstu.ru>
2023-06-23 13:18:02 +0300

    еще немного рефакторинга +есть баг, с datasource(не изменяется в процессе)

---
+++ src/main/java/project/model/AttendanceView.java
@@ -0,0 +1,44 @@
+package project.model;
+
+import lombok.*;
+import org.hibernate.annotations.Immutable;
+import org.hibernate.annotations.Subselect;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Builder
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Getter
+@Setter
+
+@Entity
+@Immutable
+@Table(name = "Attendance_view")
+@IdClass(AttendanceId.class)
+public class AttendanceView implements Serializable {
+    @Id
+    @Column
+    private String studentId;
+    @Id
+    @Column
+    private Integer lessonId;
+    @Column(name = "name")
+    private String studentName;
+    @Column
+    private Date date;
+    @Column
+    private boolean success;
+    @Column
+    private String topic;
+    @Column
+    private double duration;
+    @Column
+    private String teacher;
+@Column
+    private String subdepartment;
+
+
+}
