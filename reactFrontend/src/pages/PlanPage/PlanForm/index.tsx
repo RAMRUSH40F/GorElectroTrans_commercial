bbcea31f80a125c46f71902592dee3be16a7e011
Kirill Darenskii <kdarenskii@mail.ru>
2024-04-18 15:19:04 +0300

    fix: fixed incorrect words spelling

--- reactFrontend/src/pages/PlanPage/PlanForm/index.tsx
+++ reactFrontend/src/pages/PlanPage/PlanForm/index.tsx
@@ -28,7 +28,7 @@ const teacherOptions: DropdownOption[] = [
     { label: "Руководитель/зам.руководителя", value: "post-1" },
     { label: "Ст. мастер/мастер", value: "post-2" },
     { label: "Наставник", value: "post-3" },
-    { label: "Cпециалист по техническому обучению", value: "post-4" },
+    { label: "Специалист по техническому обучению", value: "post-4" },
     { label: "Другое", value: "post-5" },
 ];
 const statusOptions: DropdownOption[] = [
