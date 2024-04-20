78e743d0c8c38ff2c981909c68dc09b44fc82572
Kirill Darenskii <kdarenskii@mail.ru>
2024-04-20 10:05:14 +0300

    chore: extended table cell props

--- reactFrontend/src/components/Table/TableBodyRow/TableBodyCell/index.tsx
+++ reactFrontend/src/components/Table/TableBodyRow/TableBodyCell/index.tsx
@@ -4,18 +4,13 @@ import cn from "classnames";
 
 import styles from "./styles.module.scss";
 
-type Props = {
-    children: React.ReactNode;
-    onClick?: (event: React.MouseEvent) => void;
-    className?: string;
-};
+type TableBodyCellProps = React.HTMLAttributes<HTMLTableCellElement>;
 
-const TableBodyCell: React.FC<Props> = ({ children, onClick, className }) => {
-    return (
-        <td className={cn(styles.bodyCell, className)} onClick={onClick}>
-            {children}
-        </td>
-    );
+const TableBodyCell: React.FC<TableBodyCellProps> = ({
+    className,
+    ...props
+}) => {
+    return <td className={cn(styles.bodyCell, className)} {...props} />;
 };
 
 export default TableBodyCell;
