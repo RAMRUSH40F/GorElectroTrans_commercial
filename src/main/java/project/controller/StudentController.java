5a387277dd4172b815885a816a2d417b776dd88b
kust <cug.s.il@yandex.ru>
2024-06-20 15:29:11 +0300

    [StudentImport] Загрузка студентов из Excel файла

--- src/main/java/project/controller/StudentController.java
+++ src/main/java/project/controller/StudentController.java
@@ -2,16 +2,24 @@ package project.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import project.model.ImportStudentResponse;
 import project.model.Student;
 import project.model.projection.StudentIdName;
+import project.service.StudentImportServiceImpl;
 import project.service.StudentServiceImpl;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,6 +31,7 @@ import static project.exceptions.Validator.*;
 public class StudentController {
 
     private final StudentServiceImpl studentService;
+    private final StudentImportServiceImpl studentImportService;
 
     @GetMapping("/dep_{N}/students/data")
     public ResponseEntity<List<Student>> findStudents(@PathVariable("N") String depId,
@@ -42,9 +51,9 @@ public class StudentController {
 
     @GetMapping("/dep_{N}/students/getByName")
     public ResponseEntity<List<StudentIdName>> findStudentsByName(@PathVariable("N") String depId,
-                                                            @RequestParam String key,
-                                                            @RequestParam(required = false) Integer limit,
-                                                            @RequestHeader(value = HttpHeaders.AUTHORIZATION, defaultValue = "") String jwtToken) {
+                                                                  @RequestParam String key,
+                                                                  @RequestParam(required = false) Integer limit,
+                                                                  @RequestHeader(value = HttpHeaders.AUTHORIZATION, defaultValue = "") String jwtToken) {
         int departmentId = validateDepartmentId(depId);
         List<StudentIdName> students = studentService.findByNameContains(departmentId, key, limit);
 
@@ -85,5 +94,46 @@ public class StudentController {
         Integer departmentId = validateDepartmentId(depId);
         validateStudentId(studentId);
         studentService.deleteStudentById(departmentId, studentId);
+    }
+
+    @GetMapping("/dep_{N}/students/studentAddTemplate")
+    public ResponseEntity<ByteArrayResource> getStudentAddTemplate(@PathVariable("N") String depId,
+                                                                   @RequestHeader(value = HttpHeaders.AUTHORIZATION, defaultValue = "") String jwtToken) {
+        Integer departmentId = validateDepartmentId(depId);
+
+        XSSFWorkbook reportFile = studentImportService.getStudentImportTemplate();
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        try {
+            reportFile.write(bos);
+        } catch (IOException exception) {
+            throw new RuntimeException("Ошибка записи в поток байт");
+        }
+
+        byte[] bytes = bos.toByteArray();
+
+        // Set response headers
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+        headers.setContentDispositionFormData("attachment", "Шаблон_загрузки_студентов.xlsx");
+
+        // Return ResponseEntity with file data and headers
+        return ResponseEntity.ok()
+                .headers(headers)
+                .body(new ByteArrayResource(bytes));
+    }
+
+    @PostMapping("/dep_{N}/students/importStudent")
+    public ResponseEntity<ImportStudentResponse> saveStudentFromExcel(@PathVariable("N") String depId,
+                                                                      @RequestParam("file") MultipartFile file,
+                                                                      @RequestHeader(value = HttpHeaders.AUTHORIZATION, defaultValue = "") String jwtToken) {
+        Integer departmentId = validateDepartmentId(depId);
+        ImportStudentResponse students;
+        try {
+            XSSFWorkbook workbook = new XSSFWorkbook(file.getInputStream());
+            students = studentImportService.importStudents(workbook, departmentId);
+        } catch (IOException exception) {
+            throw new RuntimeException("Ошибка чтения файла");
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).body(students);
     }
 }
