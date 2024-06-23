899de6a71e6d7e0a63c33d54e9b48aa805c04018
ArtemShev <artem.shevtsov.1996@mail.ru>
2024-06-23 22:28:40 +0300

    [Reports] Update: now supports xlsx format and dateFrom dateTo params

--- src/main/java/project/service/reportService/ReportService.java
+++ src/main/java/project/service/reportService/ReportService.java
@@ -1,21 +1,22 @@
 package project.service.reportService;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.poi.hssf.usermodel.HSSFCell;
-import org.apache.poi.hssf.usermodel.HSSFRow;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.Font;
 import org.apache.poi.ss.usermodel.IndexedColors;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.jetbrains.annotations.NotNull;
-import org.springframework.data.util.Pair;
 import org.springframework.stereotype.Service;
 import project.model.QuarterDateModel;
 import project.repository.ReportRepository;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.time.LocalDate;
 import java.time.Year;
@@ -24,76 +25,101 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
+import java.util.function.Supplier;
 
 import static project.dataSource.DynamicDataSourceContextHolder.setCurrentDataSource;
 
 
 @Service("ReportServiceBean")
 @Slf4j
-@RequiredArgsConstructor
 public class ReportService {
-    private static final String REPORT_TEMPLATE_PATH = "/report_template.xls";
-
+    private static final String REPORT_TEMPLATE_PATH = "/report_template.xlsx";
+    private final static int SHEET_INDEX = 0;
     private final ReportRepository reportRepository;
+    private CellStyle cellStyle;
+
+    public ReportService(ReportRepository reportRepository) {
+        this.reportRepository = reportRepository;
+    }
 
 
     @NotNull
-    public HSSFWorkbook createReport(int quarter, int year) {
+    public ByteArrayOutputStream createReport(LocalDate dateFrom, LocalDate dateTo) {
         log.info("Начали создание отчета");
-        Pair<Integer, Integer> yearMonthPair = calculateDate(quarter, year);
-        HSSFWorkbook hssfWorkBook = createWorkbook();
-        HSSFSheet sheet = hssfWorkBook.getSheet("Лист1");
-        createLessonReportPart(sheet, yearMonthPair);
-        createWorkerReportPart(sheet, yearMonthPair);
-        createTeacherReportPart(sheet, yearMonthPair);
+        XSSFWorkbook xssfWorkBook = createWorkbook();
+        cellStyle = applyClassicStyle(xssfWorkBook);
+        XSSFSheet sheet = xssfWorkBook.getSheetAt(SHEET_INDEX);
+        createLessonReportPart(sheet, dateFrom, dateTo);
+        createWorkerReportPart(sheet, dateFrom, dateTo);
+        createTeacherReportPart(sheet, dateFrom, dateTo);
         log.info("Закончили создание отчета");
-        return hssfWorkBook;
+        return convertWorkbookToByteArray(xssfWorkBook);
     }
 
-    private HSSFWorkbook createWorkbook() {
+    private XSSFWorkbook createWorkbook() {
         try (InputStream inputStream = Objects.requireNonNull(getClass().getResourceAsStream(REPORT_TEMPLATE_PATH))) {
-            return new HSSFWorkbook(inputStream);
+            return new XSSFWorkbook(inputStream);
         } catch (Exception e) {
             log.error("Ошибка на этапе чтения файла:", e);
             throw new RuntimeException("Файл шаблона не был загружен в корневую папку проекта или " +
                     "произошла другая ошибка связанная с чтением шаблонной таблицы", e);
         }
     }
-    private void createLessonReportPart(HSSFSheet sheet, Pair<Integer, Integer> yearMonthPair) {
-        setRowValues(sheet.getRow(2), () -> reportRepository.findAllLessonsBetweenDates(yearMonthPair.getFirst(), yearMonthPair.getSecond()));
-        setRowValues(sheet.getRow(3), () -> reportRepository.findAllLessonsBetweenDatesWithHeld(yearMonthPair.getFirst(), yearMonthPair.getSecond()));
-        setRowValues(sheet.getRow(4), () -> reportRepository.findAllLessonsBetweenDates(yearMonthPair.getFirst(), yearMonthPair.getSecond()));
-        setRowValues(sheet.getRow(5), () -> reportRepository.findAllLessonsBetweenDatesWithHeld(yearMonthPair.getFirst(), yearMonthPair.getSecond()));
+
+    private ByteArrayOutputStream convertWorkbookToByteArray(XSSFWorkbook workbook) {
+        try (ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream()) {
+            workbook.write(byteArrayOutputStream);
+            return byteArrayOutputStream;
+        } catch (IOException e) {
+            log.info("Ошибка при конвертации отчета в массив байтов");
+            throw new RuntimeException(e);
+        }
     }
 
-    private void createWorkerReportPart(HSSFSheet sheet, Pair<Integer, Integer> yearMonthPair) {
-        setRowValues(sheet.getRow(6), () -> reportRepository.findAllWorkersBetweenDates(yearMonthPair.getFirst(), yearMonthPair.getSecond()));
-        setRowValues(sheet.getRow(7), () -> reportRepository.findAllWorkersBetweenDatesWithSuccess(yearMonthPair.getFirst(), yearMonthPair.getSecond()));
-        int rowNumber = 8;
+    /**
+     * Списко изменений: поменять yearMounthPair на LocalDate
+     * 1) Так же будет приходить Пара дат, для этого можно создать отдельный класс
+     * 2) Убрать лямды, сразу пероедавать значение - лямды лишние
+     *
+     * @param sheet
+     * @param yearMonthPair
+     */
+    private void createLessonReportPart(XSSFSheet sheet, LocalDate dateForm, LocalDate dateTo) {//
+        setRowValues(sheet.getRow(2), ()->reportRepository.findAllLessonsBetweenDates(dateForm, dateTo));
+        setRowValues(sheet.getRow(3), ()->reportRepository.findAllLessonsBetweenDatesWithHeld(dateForm, dateTo));
+        setRowValues(sheet.getRow(4), ()->reportRepository.findAllLessonsBetweenDates(dateForm, dateTo));
+        setRowValues(sheet.getRow(5), ()->reportRepository.findAllLessonsBetweenDatesWithHeld(dateForm, dateTo));
+    }
+
+    private void createWorkerReportPart(XSSFSheet sheet, LocalDate dateForm, LocalDate dateTo) {
+        setRowValues(sheet.getRow(6), ()->reportRepository.findAllWorkersBetweenDates(dateForm, dateTo));
+        setRowValues(sheet.getRow(7), ()->reportRepository.findAllWorkersBetweenDatesWithSuccess(dateForm, dateTo));
+        int rowNumber = 8;// тут что-то не тако
         for (WorkerProfessions profession : WorkerProfessions.values()) {
-            setRowValues(sheet.getRow(rowNumber), () -> reportRepository.findAllWorkersBetweenDatesWithSuccessAndProfession(yearMonthPair.getFirst(), yearMonthPair.getSecond(), profession.getProfession()));
+            setRowValues(sheet.getRow(rowNumber), ()->reportRepository.findAllWorkersBetweenDatesWithSuccessAndProfession(dateForm, dateTo, profession.getProfession()));
             rowNumber++;
         }
         formStatsForOthersCategory(sheet.getRow(13), 7, 12, sheet);
     }
 
-    private void createTeacherReportPart(HSSFSheet sheet, Pair<Integer, Integer> yearMonthPair) {
-        setRowValues(sheet.getRow(14), () -> reportRepository.findAllLessonsBetweenDates(yearMonthPair.getFirst(), yearMonthPair.getSecond()));
+    private void createTeacherReportPart(XSSFSheet sheet, LocalDate dateForm, LocalDate dateTo) {
+        setRowValues(sheet.getRow(14), ()->reportRepository.findAllLessonsBetweenDates(dateForm, dateTo));
         int rowNumber = 15;
         for (TeacherProfession profession : TeacherProfession.values()) {
-            setRowValues(sheet.getRow(rowNumber), () -> reportRepository.findAllTeachersByProfession(yearMonthPair.getFirst(), yearMonthPair.getSecond(), profession.getProfession()));
+            setRowValues(sheet.getRow(rowNumber), ()->reportRepository.findAllTeachersByProfession(dateForm, dateTo, profession.getProfession()));
             rowNumber++;
         }
         formStatsForOthersCategory(sheet.getRow(18), 14, 17, sheet);
     }
 
-    private void formStatsForOthersCategory(HSSFRow destination, int from, int to, HSSFSheet sheet) {
+    //Попробовать заменить на функцию в exel, либо просто создать запрос в бд соотвесвующий,
+// логика на стороне сервиса - излишняя
+    private void formStatsForOthersCategory(XSSFRow destination, int from, int to, XSSFSheet sheet) {
         int value;
         int firstCell = 1;
         int lastCell = 31;
-        HSSFRow row;
+        XSSFRow row;
         Cell cell;
-        CellStyle style = applyClassicStyle(destination.getSheet().getWorkbook());
         int result;
         for (int cellIndex = firstCell; cellIndex < lastCell; cellIndex += 2) {
             value = 0;
@@ -103,26 +129,23 @@ public class ReportService {
             }
             result = (int) sheet.getRow(from).getCell(cellIndex).getNumericCellValue() - value;
             cell = destination.getCell(cellIndex);
-            cell.setCellStyle(style);
+            cell.setCellStyle(cellStyle);
             cell.setCellValue(result);
         }
         setResultingRowValues(destination);
     }
 
-
+    //Уже будет не нужно
     /*
      * Расчет интервала
      */
-    private Pair<Integer, Integer> calculateDate(int quarter, int year) {
-        int month;
-        month = (quarter - 1) * 3 + 1;
-        return Pair.of(year, month);
-    }
+
 
     /*
      * Формирование стиля
      */
-    private CellStyle applyClassicStyle(HSSFWorkbook workbook) {
+    //Вынести стиль в класс, применять один раз
+    private CellStyle applyClassicStyle(XSSFWorkbook workbook) {
         CellStyle style = workbook.createCellStyle();
         // Настройки фона и шрифта
         Font font = workbook.createFont();
@@ -136,6 +159,7 @@ public class ReportService {
         return style;
     }
 
+    //Будет не нужно
     @NotNull
     public List<QuarterDateModel> getQuarterDateModelList() {
         int year = Year.now().getValue();
@@ -152,23 +176,24 @@ public class ReportService {
         return intervals;
     }
 
-    private void setRowValues(HSSFRow row, StatisticFormator statisticFormator) {
-        HSSFCell cell;
+    //Убрать все магически числа
+    //Красивее сделать переключение между датасорсами
+    private void setRowValues(XSSFRow row, Supplier<Integer> supplier) {
+        XSSFCell cell;
         int lastCell = 31;
-        CellStyle style = applyClassicStyle(row.getSheet().getWorkbook());
         for (int column = 1; column < lastCell - 1; column += 2) {
             setCurrentDataSource("DEP_" + (column / 2 + 1));
             cell = row.getCell(column);
-            cell.setCellStyle(style);
-            cell.setCellValue(statisticFormator.formStatistic());
+            cell.setCellStyle(cellStyle);
+            cell.setCellValue(supplier.get());
         }
-        setResultingRowValues(row);
+        //setResultingRowValues(row);
     }
 
-    private void setResultingRowValues(HSSFRow row) {
+    //Попробовать заменить на exel функцию
+    private void setResultingRowValues(XSSFRow row) {
         int total = 0;
         Cell cell;
-        CellStyle style = applyClassicStyle(row.getSheet().getWorkbook());
 
         for (Iterator<Cell> x = row.cellIterator(); x.hasNext(); ) {
             cell = x.next();
@@ -176,7 +201,7 @@ public class ReportService {
                 continue;
             }
             if (cell.getColumnIndex() == row.getLastCellNum() - 2) {
-                cell.setCellStyle(style);
+                cell.setCellStyle(cellStyle);
                 cell.setCellValue(total);
             }
             total += cell.getNumericCellValue();
