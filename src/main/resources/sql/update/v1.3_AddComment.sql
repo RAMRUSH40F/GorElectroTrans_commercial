d4a7e92e7d2d8cc96ee41f91c389c7c3d035d3f6
rahimkulov <rahimkulov@yoomoney.ru>
2024-06-24 22:15:22 +0300

    [LessonCommentStatus] Изменен тип comment на varchar(100), так как среднеяя длина комментария 30-40 символов

--- src/main/resources/sql/update/v1.3_AddComment.sql
+++ src/main/resources/sql/update/v1.3_AddComment.sql
@@ -1,47 +1,47 @@
 
 USE DEP_1;
 ALTER TABLE lesson
-    ADD comment varchar(1000);
+    ADD comment varchar(100);
 USE DEP_2;
 ALTER TABLE lesson
-    ADD comment varchar(1000);
+    ADD comment varchar(100);
 USE DEP_3;
 ALTER TABLE lesson
-    ADD comment varchar(1000);
+    ADD comment varchar(100);
 USE DEP_4;
 ALTER TABLE lesson
-    ADD comment varchar(1000);
+    ADD comment varchar(100);
 USE DEP_5;
 ALTER TABLE lesson
-    ADD comment varchar(1000);
+    ADD comment varchar(100);
 USE DEP_6;
 ALTER TABLE lesson
-    ADD comment varchar(1000);
+    ADD comment varchar(100);
 USE DEP_7;
 ALTER TABLE lesson
-    ADD comment varchar(1000);
+    ADD comment varchar(100);
 USE DEP_8;
 ALTER TABLE lesson
-    ADD comment varchar(1000);
+    ADD comment varchar(100);
 USE DEP_9;
 ALTER TABLE lesson
-    ADD comment varchar(1000);
+    ADD comment varchar(100);
 USE DEP_10;
 ALTER TABLE lesson
-    ADD comment varchar(1000);
+    ADD comment varchar(100);
 USE DEP_11;
 ALTER TABLE lesson
-    ADD comment varchar(1000);
+    ADD comment varchar(100);
 USE DEP_12;
 ALTER TABLE lesson
-    ADD comment varchar(1000);
+    ADD comment varchar(100);
 USE DEP_13;
 ALTER TABLE lesson
-    ADD comment varchar(1000);
+    ADD comment varchar(100);
 USE DEP_14;
 ALTER TABLE lesson
-    ADD comment varchar(1000);
+    ADD comment varchar(100);
 USE DEP_15;
 ALTER TABLE lesson
-    ADD comment varchar(1000);
+    ADD comment varchar(100);
     
