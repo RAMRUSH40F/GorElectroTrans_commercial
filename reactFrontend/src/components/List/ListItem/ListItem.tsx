8727b06031d982b01097b45932892649f326d671
Darensky Kirill <darensky_kirill@rocknblock.io>
2024-06-25 18:13:45 +0300

    style: change styling of list item

--- reactFrontend/src/components/List/ListItem/ListItem.tsx
+++ reactFrontend/src/components/List/ListItem/ListItem.tsx
@@ -6,18 +6,22 @@ import styles from "./listItem.module.scss";
 
 interface ListItemProps extends HTMLAttributes<HTMLLIElement> {
     selected?: boolean;
+    disabled?: boolean;
 }
 
 export const ListItem: FC<ListItemProps> = ({
     className,
     selected,
+    disabled,
     ...props
 }) => {
+    console.log(disabled);
     return (
         <li
             className={cn(
                 styles.listItem,
                 selected && styles.selected,
+                disabled && styles.disabled,
                 className,
             )}
             {...props}
