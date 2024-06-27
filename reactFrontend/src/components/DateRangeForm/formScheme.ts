b16745ffb3a64c8cfe1eb1559784e8c18b8502bf
Darensky Kirill <darensky_kirill@rocknblock.io>
2024-06-27 21:59:19 +0300

    feat: change date range form validation

--- reactFrontend/src/components/DateRangeForm/formScheme.ts
+++ reactFrontend/src/components/DateRangeForm/formScheme.ts
@@ -1,10 +1,12 @@
 import * as yup from "yup";
 
+const DAY_MS = 24 * 60 * 60 * 1000;
+
 export const dateValidationSchema = yup.object().shape({
     startDate: yup
         .date()
         .required("Обязательное поле")
-        .max(new Date(), "Дата еще не наступила")
+        .max(new Date(new Date().getTime() + DAY_MS), "Дата еще не наступила")
         .typeError("Некорректная дата"),
     endDate: yup
         .date()
