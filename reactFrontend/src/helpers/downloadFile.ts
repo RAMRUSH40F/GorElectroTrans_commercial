6238b7073672c5cc16f807f11c2f45e6e70c393a
Darensky Kirill <darensky_kirill@rocknblock.io>
2024-06-29 16:32:56 +0300

    feat: add container param to downloadFileFx

--- reactFrontend/src/helpers/downloadFile.ts
+++ reactFrontend/src/helpers/downloadFile.ts
@@ -3,16 +3,17 @@ import { createEffect } from "effector";
 interface DownloadParams {
     file: Blob;
     fileName: string;
+    container?: HTMLElement;
 }
 
 export const downloadFileFx = createEffect<DownloadParams, string>(
-    ({ file, fileName }) => {
+    ({ file, fileName, container = document.body }) => {
         const url = URL.createObjectURL(file);
         const link = document.createElement("a");
         link.href = url;
         link.setAttribute("download", fileName);
-        document.body.append(link);
         link.style.display = "none";
+        container.append(link);
         link.click();
         link.remove();
         URL.revokeObjectURL(url);
