05c801ed9855d5666024b47f30b229ad9bdd9229
Darensky Kirill <darensky_kirill@rocknblock.io>
2024-06-29 15:57:42 +0300

    feat: add logic for employees uploading

---
+++ reactFrontend/src/pages/EmployeesPage/EmployeesUploading/model.ts
@@ -0,0 +1,108 @@
+import { ChangeEvent } from "react";
+
+import { attach, createDomain, sample } from "effector";
+
+import { downloadFileFx } from "helpers/downloadFile";
+import { NOTICE, showNoticeFx } from "helpers/notice";
+
+import employeeApi from "shared/api/employeesApi";
+import { AbortParams } from "shared/api/types";
+
+import { $depId } from "../model";
+
+const domain = createDomain();
+
+export const errorReset = domain.createEvent();
+
+export const modalOpened = domain.createEvent();
+export const modalClosed = domain.createEvent();
+
+export const fileChanged = domain.createEvent<ChangeEvent<HTMLInputElement>>();
+
+export const backButtonClicked = domain.createEvent();
+export const downloadButtonClicked = domain.createEvent();
+export const uploadButtonClicked = domain.createEvent();
+
+export const $file = domain.createStore<File | null>(null);
+
+export const $isUploading = domain.createStore(false);
+export const $isTemplateLoading = domain.createStore(false);
+
+export const $error = domain.createStore<string | null>(null);
+
+export const $isModalActive = domain.createStore<boolean>(false);
+
+export const getTemplateFx = attach({ effect: employeeApi.fetchTemplateFx });
+
+export const uploadEmployeesFx = attach({
+    effect: employeeApi.uploadEmployeesFx,
+    source: { depId: $depId },
+    mapParams({ data, controller }: AbortParams<FormData>, { depId }) {
+        return { depId, data, controller };
+    },
+});
+
+sample({
+    clock: downloadButtonClicked,
+    source: { depId: $depId },
+    fn: ({ depId }) => {
+        return {
+            depId,
+            data: null,
+            controller: new AbortController(),
+        };
+    },
+    target: getTemplateFx,
+});
+
+sample({
+    clock: getTemplateFx.doneData,
+    fn: (file) => ({
+        file,
+        fileName: `Шаблон_сотрудников.xlsx`,
+    }),
+    target: downloadFileFx,
+});
+
+sample({
+    clock: uploadButtonClicked,
+    source: { file: $file },
+    filter: ({ file }) => file !== null,
+    fn: ({ file }) => {
+        const formData = new FormData();
+        formData.append("file", file!);
+        return { data: formData, controller: new AbortController() };
+    },
+    target: uploadEmployeesFx,
+});
+
+sample({
+    clock: uploadEmployeesFx.done,
+    fn: (_) => ({
+        type: NOTICE.SUCCESS,
+        message: "Работники успешно загружены",
+    }),
+    target: showNoticeFx,
+});
+
+domain.onCreateStore(($store) => {
+    $store.reset(backButtonClicked, modalClosed);
+});
+
+$file
+    .on(fileChanged, (_, event) => {
+        const filesList = event.target.files;
+        const file = filesList ? filesList[0] : null;
+        return file;
+    })
+    .reset(uploadEmployeesFx.done);
+
+$isUploading.on(uploadEmployeesFx.pending, (_, pending) => pending);
+
+$error
+    .on([uploadEmployeesFx.failData, getTemplateFx.failData], (_, error) =>
+        error.isCanceled ? null : error.message,
+    )
+    .reset(errorReset, uploadEmployeesFx, getTemplateFx, modalClosed);
+
+$isModalActive.on(modalOpened, () => true).on(modalClosed, () => false);
