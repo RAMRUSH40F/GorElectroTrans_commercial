e889134657bcafe1ddc87d88050f3863e06ea185
Darensky Kirill <darensky_kirill@rocknblock.io>
2024-06-29 15:56:15 +0300

    feat: add uploadEmployees api effect

--- reactFrontend/src/shared/api/employeesApi/index.ts
+++ reactFrontend/src/shared/api/employeesApi/index.ts
@@ -1,9 +1,19 @@
 import { deleteFx } from "./delete";
 import { fetchFx } from "./fetch";
 import { fetchByNameFx } from "./fetchByName";
+import { fetchTemplateFx } from "./fetchTemplate";
 import { postFx } from "./post";
 import { putFx } from "./put";
+import { uploadEmployeesFx } from "./uploadEmployees";
 
-const employeeApi = { fetchFx, fetchByNameFx, postFx, putFx, deleteFx };
+const employeeApi = {
+    fetchFx,
+    fetchByNameFx,
+    postFx,
+    putFx,
+    deleteFx,
+    fetchTemplateFx,
+    uploadEmployeesFx,
+};
 
 export default employeeApi;
---
+++ reactFrontend/src/shared/api/employeesApi/uploadEmployees.ts
@@ -0,0 +1,40 @@
+import { isCancel } from "axios";
+import { createEffect } from "effector";
+
+import { IMaterial } from "models/Plan";
+
+import { authRequestFx } from "..";
+import { ApiError, AuthError, DepParams } from "../types";
+
+export const uploadEmployeesFx = createEffect<
+    DepParams<FormData>,
+    IMaterial,
+    ApiError
+>(async ({ depId, data, controller }) => {
+    try {
+        const response = await authRequestFx({
+            method: "POST",
+            url: `/dep_${depId}/students/importStudent`,
+            data,
+            headers: {
+                "Content-Type": "multipart/form-data",
+            },
+            signal: controller.signal,
+        });
+        return response.data as IMaterial;
+    } catch (error) {
+        if (isCancel(error)) {
+            const customError: ApiError = {
+                message: "Запрос отменен",
+                isCanceled: true,
+            };
+            throw customError;
+        } else {
+            const err = error as AuthError;
+            const message =
+                err?.response?.data?.message ?? "Не удалось загрузить файл";
+            const customError: ApiError = { message, isCanceled: false };
+            throw customError;
+        }
+    }
+});
