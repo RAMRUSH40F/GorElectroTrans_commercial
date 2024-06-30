f4bb6e43d8ef6df81e40ce8b4821d89c23650ce8
Darensky Kirill <darensky_kirill@rocknblock.io>
2024-06-30 19:23:02 +0300

    feat: replace previous form with date range form

--- reactFrontend/src/pages/PlanPage/PlanReport/index.tsx
+++ reactFrontend/src/pages/PlanPage/PlanReport/index.tsx
@@ -2,11 +2,9 @@ import React, { useRef } from "react";
 
 import { useUnit } from "effector-react";
 
-import PlanReportForm from "pages/PlanPage/PlanReport/PlanReportForm";
-
 import ActionButton from "components/ActionButton";
 import Alert, { ALERT } from "components/Alert";
-import Loader from "components/Loader";
+import { DateRangeForm } from "components/DateRangeForm";
 import ModalLayout from "components/ModalLayout";
 import ModalContent from "components/ModalLayout/ModalContent";
 import ModalHeader from "components/ModalLayout/ModalHeader";
@@ -16,10 +14,10 @@ import useEscape from "hooks/useEscape";
 import useLockedBody from "hooks/useLockedBody";
 
 import {
+    $error,
     $isLoading,
     $isModalActive,
-    $periods,
-    $periodsError,
+    formSubmitted,
     modalClosed,
     modalOpened,
 } from "./model";
@@ -44,7 +42,7 @@ const PlanReport: React.FC = () => {
 export default PlanReport;
 
 function ReportModal() {
-    const isModalActive = useUnit($isModalActive);
+    const [isModalActive, isLoading] = useUnit([$isModalActive, $isLoading]);
 
     const modalRef = useRef<HTMLDivElement | null>(null);
     useClickOutside(modalRef, () => modalClosed());
@@ -60,45 +58,23 @@ function ReportModal() {
             </ModalHeader>
             <ModalContent>
                 <ErrorAlert />
-                <EmptyAlert />
-                <Loading />
-                <Form />
+                <DateRangeForm
+                    onSubmit={formSubmitted}
+                    isDisabled={isLoading}
+                />
             </ModalContent>
         </ModalLayout>
     );
 }
 
-function Form() {
-    const [periods, periodsError] = useUnit([$periods, $periodsError]);
-    if (periodsError || periods.length === 0) return null;
-    return <PlanReportForm />;
-}
-
-function EmptyAlert() {
-    const [periods, periodsError, isLoading] = useUnit([
-        $periods,
-        $periodsError,
-        $isLoading,
-    ]);
-    if (periods.length > 0 || periodsError !== null || isLoading) return null;
-    return (
-        <Alert className={styles.alert} type={ALERT.INFO}>
-            Нет информации о кварталах
-        </Alert>
-    );
-}
-
 function ErrorAlert() {
-    const periodsError = useUnit($periodsError);
-    if (!periodsError) return null;
+    const error = useUnit($error);
+
+    if (!error) return null;
+
     return (
         <Alert className={styles.alert} type={ALERT.ERROR}>
-            {periodsError}
+            {error}
         </Alert>
     );
-}
-
-function Loading() {
-    const isLoading = useUnit($isLoading);
-    return isLoading ? <Loader className={styles.loader} /> : null;
 }
