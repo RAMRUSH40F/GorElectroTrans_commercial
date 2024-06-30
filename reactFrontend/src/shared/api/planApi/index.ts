ce34e36a5053fadf60f46ff7897ccea323f1a0b1
Darensky Kirill <darensky_kirill@rocknblock.io>
2024-06-30 19:21:20 +0300

    feat: add updated fetchReport api effect with date range params

--- reactFrontend/src/shared/api/planApi/index.ts
+++ reactFrontend/src/shared/api/planApi/index.ts
@@ -1,8 +1,9 @@
 import { deleteFx } from "./delete";
 import { fetchFx } from "./fetch";
+import { fetchPlanReportFx } from "./fetchReport";
 import { postFx } from "./post";
 import { putFx } from "./put";
 
-const planApi = { fetchFx, postFx, putFx, deleteFx };
+const planApi = { fetchFx, postFx, putFx, deleteFx, fetchPlanReportFx };
 
 export default planApi;
