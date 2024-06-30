16ef051bfdcfd250a7f946f7c0d6fdb72ccd20fe
Darensky Kirill <darensky_kirill@rocknblock.io>
2024-06-30 19:35:31 +0300

    chore: autoformat files

--- reactFrontend/src/shared/api/planApi/fetchReport.ts
+++ reactFrontend/src/shared/api/planApi/fetchReport.ts
@@ -11,37 +11,39 @@ interface ReportResponse {
 
 interface ReportParams extends DepParams<null>, DateRange {}
 
-export const fetchPlanReportFx = createEffect<ReportParams, ReportResponse, ApiError>(
-    async ({ depId, controller, dateFrom, dateTo }) => {
-        try {
-            const response = await authRequestFx({
-                method: "GET",
-                url: `/dep_${depId}/report/stats`,
-                responseType: "blob",
-                headers: {
-                    "Content-Type": "application/octet-stream",
-                },
-                params: { dateFrom, dateTo },
-                signal: controller.signal,
-            });
-            return {
-                file: response.data as Blob,
-                dateRange: { dateFrom, dateTo },
+export const fetchPlanReportFx = createEffect<
+    ReportParams,
+    ReportResponse,
+    ApiError
+>(async ({ depId, controller, dateFrom, dateTo }) => {
+    try {
+        const response = await authRequestFx({
+            method: "GET",
+            url: `/dep_${depId}/report/stats`,
+            responseType: "blob",
+            headers: {
+                "Content-Type": "application/octet-stream",
+            },
+            params: { dateFrom, dateTo },
+            signal: controller.signal,
+        });
+        return {
+            file: response.data as Blob,
+            dateRange: { dateFrom, dateTo },
+        };
+    } catch (error) {
+        if (isCancel(error)) {
+            const customError: ApiError = {
+                message: "Запрос отменен",
+                isCanceled: true,
             };
-        } catch (error) {
-            if (isCancel(error)) {
-                const customError: ApiError = {
-                    message: "Запрос отменен",
-                    isCanceled: true,
-                };
-                throw customError;
-            } else {
-                const err = error as AuthError;
-                const message =
-                    err?.response?.data?.message ?? "Не удалось скачать отчет";
-                const customError: ApiError = { message, isCanceled: false };
-                throw customError;
-            }
+            throw customError;
+        } else {
+            const err = error as AuthError;
+            const message =
+                err?.response?.data?.message ?? "Не удалось скачать отчет";
+            const customError: ApiError = { message, isCanceled: false };
+            throw customError;
         }
-    },
-);
+    }
+});
